047450689ee4e5168f82095f42eaab1347412510
l.guerra--rago <l.guerra--rago@xefi.fr>
2024-11-22 13:11:30 +0100

    wip

---
+++ tests/src/index.ts
@@ -0,0 +1,15 @@
+import {defineApi} from "../../src/request/registry/defineApi";
+
+console.log("test")
+
+// url
+// method
+// base url
+// headers
+// staleTime
+// propsScheme
+// returnScheme
+//
+// propsType : post->body|data get->url
+
+defineApi()
